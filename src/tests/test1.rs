3b0189b7a594e9a2e8139567abaf1b5dc073c930
dbartilson@tutanota.com <dbartilson>
2024-05-28 18:34:21 -0600

    add json and vtk input tests

--- src/tests/test1.rs
+++ src/tests/test1.rs
@@ -1,7 +1,19 @@
 
 
 #[test]
-fn it_adds_two() {
-    cc_abstemious::run();
-    assert_eq!(3, 5);
+fn json_reader() {
+    use cc_abstemious::preprocess::input_data::*;
+    let u = read_input_json("./src/tests/input_1.json").unwrap();
+    assert_eq!(u.body_index, 3);
+}
+
+#[test]
+fn vtk_reader() {
+    use cc_abstemious::preprocess::*;
+    use std::path::Path;
+
+    let mut mesh: mesh_data::Mesh = Default::default();
+    let _result = mesh.read_from_vtk(Path::new("./src/tests/mesh_1.vtk"));
+
+    assert_eq!(mesh.elements.len(), 336);
 }
