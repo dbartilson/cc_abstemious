3e5e8257d5c1d29c654258da54570fcd240284b7
dbartilson@tutanota.com <dbartilson>
2024-06-27 11:43:41 -0600

    improvements to gmres

--- src/solve/gmres.rs
+++ src/solve/gmres.rs
@@ -48,19 +48,18 @@ fn gmres(a: &DMatrix::<Cplx>, x: &mut DVector<Cplx>, b: &DVector<Cplx>,
     let mut e = vec![error];
     let r_norm = r.norm();
     let mut q = DMatrix::<Cplx>::from_element(n, m+1, c_zero);
-    q.set_column(0, &r);
-    q.scale_mut(1.0 / r_norm);
+    q.set_column(0, &r.normalize());
     let mut beta = e1.clone().scale(r_norm);
     let mut h = DMatrix::<Cplx>::from_element(n, m, c_zero);
     let mut kk: usize = 0;
     for k in 0..m {
+        kk = k;
         println!("   Iteration: {}, Error: {}", k, error);
-        kk += 1;
         let (mut hk1, qk1) = arnoldi(a, &q, k);
+        apply_givens_rotation(&mut hk1, &mut cs, &mut sn, k);
         h.set_column(k, &hk1);
         q.set_column(k+1, &qk1);
-        apply_givens_rotation(&mut hk1, &mut cs, &mut sn, k);
-        beta[k+1] = -sn[k] * beta[k];
+        beta[k+1] = -sn[k].conj() * beta[k];
         beta[k] = cs[k] * beta[k];
         error = beta[k+1].norm() / b_norm;
         e.push(error);
@@ -73,13 +72,14 @@ fn gmres(a: &DMatrix::<Cplx>, x: &mut DVector<Cplx>, b: &DVector<Cplx>,
         else if k == m-1 {
             flag = ExitFlag::Iterations;
         }
+
     }
 
     let hkk = h.view((0, 0), (kk, kk));
     let hkklu = hkk.lu();
     let betakk = beta.rows(0, kk);
     let y = hkklu.solve(&betakk);
-    x.gemv(c_one, &q.view((0,0), (n, kk)), y.as_ref().unwrap(), c_one);
+    x.gemv(c_one, &q.columns(0,kk), y.as_ref().unwrap(), c_one);
     return flag;
 }
 
@@ -92,11 +92,14 @@ fn arnoldi(a: &DMatrix::<Cplx>, q: &DMatrix<Cplx>, k: usize)
     qk1.gemv(c_one, a, &q.column(k), c_zero);
     let mut hk1 = DVector::<Cplx>::from_element(n, c_zero);
     for i in 0..k+1 {
-        hk1[i] = qk1.dot(&q.column(i));
+        hk1[i] = qk1.dotc(&q.column(i));
         qk1.axpy(-hk1[i], &q.column(i), c_one);
     }
     hk1[k+1] = Cplx::new(qk1.norm(), 0.0);
-    qk1.scale_mut(1.0 / qk1.norm());
+    let scale = c_one / hk1[k+1];
+    for qi in &mut qk1 {
+        *qi *= scale;
+    }
     return (hk1, qk1)
 }
 
@@ -105,7 +108,7 @@ fn apply_givens_rotation(hk1: &mut DVector<Cplx>, cs: &mut DVector<Cplx>,
 
     for i in 0..k {
         let temp = cs[i] * hk1[i] + sn[i] * hk1[i+1];
-        hk1[i+1] = -sn[i] * hk1[i] + cs[i] * hk1[i+1];
+        hk1[i+1] = -sn[i].conj() * hk1[i] + cs[i] * hk1[i+1];
         hk1[i] = temp;
     }
     (cs[k], sn[k]) = givens_rotation(&hk1[k], &hk1[k+1]);
@@ -114,8 +117,11 @@ fn apply_givens_rotation(hk1: &mut DVector<Cplx>, cs: &mut DVector<Cplx>,
 }
 
 fn givens_rotation(v1: &Cplx, v2: &Cplx) -> (Cplx, Cplx) {
-    let t = (v1.norm_sqr() + v2.norm_sqr()).sqrt();
-    let cs = v1 / t;
-    let sn = v2 / t;
+    let c_zero = Cplx::new(0.0, 0.0);
+    let c_one = Cplx::new(1.0, 0.0);
+    let signf = if *v1 == c_zero {v1 / v1.norm()} else {c_one};
+    let r = (v1.norm_sqr() + v2.norm_sqr()).sqrt();
+    let cs = Cplx::new(v1.norm() / r, 0.0);
+    let sn = signf * v2.conj() / r;
     return (cs, sn)
 }
